263819c558b6fd0f1d01e54bde94e751bff6ed9a
Ardi Muntanda <ardimuntanda1@gmail.com>
2023-06-22 12:58:30 -0700

    adding credits

---
+++ scripts/credits.js
@@ -0,0 +1,7 @@
+$(document).ready(() => {
+    $('.button.credits').on('click', ()=>{
+        $('.title.main').text('About The CSU Virtual Tour ');
+        $('.starting_description').html('The University virtual tour has been redesigned by the <a href="https://www.instagram.com/csu.sdt/" class="links">CSU Software Development Team</a> and voiced by <a href="" class="links">Maria Kinyoka Mafuta</a>, all of them students gathered by Cyprus Science University in an effort to promote student initiative and creativity.');
+    })
+
+})
